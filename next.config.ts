6d9225cb1e767c796d002b01d5f2492ff252e5cd
suhartono13-beep <suhartono13@madrasah.kemenag.go.id@example.com>
2026-06-02 20:41:46 +0800

    feat: setup supabase client component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 告诉 Next.js 将网站打包为静态网页 (SPA 模式)
+  images: {
+    unoptimized: true, // 静态模式下关闭 Next.js 默认的图片优化
+  },
 };
 
 export default nextConfig;
